571e4a3d95ce3104ac9bcae026c87a2cd31de859
FH Systems <mail@fh-systems.de>
2024-06-06 13:45:22 +0200

    add some stuff

--- models/cifar.py
+++ models/cifar.py
@@ -1,7 +1,8 @@
 from config.loader import getConfig
 from models.benchmarkset import BenchmarkSet
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader, random_split
+import torch.nn as nn
 
 
 class CIFAR(BenchmarkSet):
@@ -10,27 +11,27 @@ class CIFAR(BenchmarkSet):
         self.conf = getConfig()
         self.batch_size = batch_size
         self.dataset = dataset
-
+        if self.dataset == 'cifar10':
+            dataset = datasets.CIFAR10
+            self.num_classes = 10
+        else:
+            dataset = datasets.CIFAR100
+            self.num_classes = 100
     def log(self):
         pass
     def setup(self,):
         pass
     def getDataLoader(self,):
-        if self.dataset == 'cifar10':
-            dataset = datasets.CIFAR10
-            num_classes = 10
-        else:
-            dataset = datasets.CIFAR100
-            num_classes = 100
+       
         print(self.conf["dataset"]["path"])
-        trainset = dataset(self.conf["dataset"]["path"], train=True, download=True,
+        trainset = self.dataset(self.conf["dataset"]["path"], train=True, download=True,
                        transform=transforms.Compose([
                            transforms.RandomCrop(32, padding=4),
                            transforms.RandomHorizontalFlip(),
                            transforms.ToTensor(),
                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))#https://github.com/kuangliu/pytorch-cifar/issues/19
                        ]))
-        testset = dataset(self.conf["dataset"]["path"], train=False, download=True,
+        testset = self.dataset(self.conf["dataset"]["path"], train=False, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))#https://github.com/kuangliu/pytorch-cifar/issues/19
@@ -44,5 +45,7 @@ class CIFAR(BenchmarkSet):
         test_loader = DataLoader(testset, batch_size=self.batch_size, shuffle=False, num_workers=1)
         return (train_loader ,test_loader , val_loader)
     def getAssociatedModel(self):
-        pass
+        model = models.resnet18(pretrained=False)
+        model.fc = nn.Linear(model.fc.in_features, self.num_classes)
+        return model
         
