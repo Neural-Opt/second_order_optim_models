b6465ac950e85fc5460265e43e73596304f2907e
Your Name <you@example.com>
2024-07-03 12:04:40 +0200

    rm

--- a.py
+++
@@ -1,8 +0,0 @@
-
-from sacrebleu import corpus_bleu
-import numpy as np
-reference = [['Wiederaufnahme der Sitzungsperiode']]
-hypothesis = ['Wiederaufnahme der Sitzungsperiode']
-
-bleu_score = corpus_bleu(hypothesis, reference,smooth_method='exp')
-print(f"BLEU score: {np.mean(np.array(bleu_score.precisions))}")
